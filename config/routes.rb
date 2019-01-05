d4acd515bf8b29a4bd30d2234d8f407b7790ef56
Steve <steve.rodri@gmail.com>
2019-01-05 16:54:34 -0500

    Add '/api' scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :companies
-  resources :contacts
-  resources :invoices
-  resources :clients
-  resources :events
+  scope '/api' do
+    resources :companies
+    resources :contacts
+    resources :invoices
+    resources :clients
+    resources :events
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
