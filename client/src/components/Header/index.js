d4d00c2271daea13c62eb7f7c30de6b5f719afd4
Steve <steve.rodri@gmail.com>
2019-01-07 18:34:44 -0500

    Update Login Functionality

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom'
 import logo_t from '../../images/logo_t.GIF'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -8,19 +8,37 @@ import './Header.css'
 
 library.add(faUserCircle)
 
-export default function Header(){
-  return(
-    <header>
-      <img className="logo" src={logo_t} alt='logo'/>
-      <nav>
-        <Link to='/admin/events'>Events</Link>
-        <Link to='/admin/clients'>Clients</Link>
-        <Link to='/admin/invoices'>Invoices</Link>
-        <Link to='/admin/staff'>Staff</Link>
-        <Link to='/admin/inventory'>Inventory</Link>
-        <Link to='/admin/run_sheets'>Run Sheets</Link>
-      </nav>
-      <div className="user-circle"><FontAwesomeIcon icon="user-circle" size="3x"/></div>
-    </header>
-  )
+export default class Header extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      redirectToLogin: false
+    }
+  }
+
+  logOut = () => {
+    localStorage.removeItem('google_access_token')
+    this.setState({
+      redirectToLogin: true
+    })
+    this.props.removeUser()
+  }
+
+  render(){
+    if (this.state.redirectToLogin) return (<Redirect to="/login"/>)
+    return(
+      <header>
+        <img className="logo" src={logo_t} alt='logo'/>
+        <nav>
+          <Link to='/admin/events'>Events</Link>
+          <Link to='/admin/clients'>Clients</Link>
+          <Link to='/admin/invoices'>Invoices</Link>
+          <Link to='/admin/staff'>Staff</Link>
+          <Link to='/admin/inventory'>Inventory</Link>
+          <Link to='/admin/run_sheets'>Run Sheets</Link>
+        </nav>
+        <div className="user-circle" onClick={this.logOut}><FontAwesomeIcon icon="user-circle" size="3x"/></div>
+      </header>
+    )
+  }
 }
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,25 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login'
+import { Redirect } from 'react-router-dom'
 import logo_t from '../../images/logo_t.GIF'
 import './Login.css'
 
-export default function Login(props) {
-  return (
-    <div className="login-page">
-      <div className="login-form">
-        <div className="logo-container">
-          <img className="logo" src={logo_t} alt='logo'/>
-        </div>
-        <div className='google-login-container'>
-          <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-            scope="https://www.googleapis.com/auth/calendar"
-            buttonText="Login with Google"
-            onSuccess={props.responseGoogle}
-            onFailure={props.responseGoogle}
-          />
+export default class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      redirectToDashboard: false
+    }
+  }
+
+  componentDidMount(){
+    if (this.loggedIn()) {
+      this.setState({
+        redirectToDashboard: true
+      })
+    }
+  }
+
+  loggedIn = () => {
+    const token = localStorage.getItem('google_access_token')
+    if (token) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  responseGoogle = (resp) => {
+    this.setState({
+      redirectToDashboard:true
+    })
+    if (resp.accessToken) {
+      localStorage.setItem('google_access_token',resp.accessToken)
+      localStorage.setItem('profileObj', JSON.stringify(resp.profileObj))
+    }
+  }
+
+  render(){
+    if (this.state.redirectToDashboard) return (<Redirect to="/admin" />)
+    return (
+      <div className="login-page">
+        <div className="login-form">
+          <div className="logo-container">
+            <img className="logo" src={logo_t} alt='logo'/>
+          </div>
+          <div className='google-login-container'>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              scope="https://www.googleapis.com/auth/calendar"
+              buttonText="Login with Google"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
