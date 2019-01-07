2bc57d4ac49dc4c30a869dca4a65d0ee837a3094
Steve <steve.rodri@gmail.com>
2019-01-07 10:25:05 -0500

    Add Login Component - Update App.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,2 +1,22 @@
 import React from 'react'
+import GoogleLogin from 'react-google-login'
 import './Login.css'
+
+export default function Login (props) {
+  return (
+    <div>
+      <div>
+        <img />
+      </div>
+      <div>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          scope="https://www.googleapis.com/auth/calendar"
+          buttonText="Login with Google"
+          onSuccess={props.responseGoogle}
+          onFailure={props.responseGoogle}
+        />
+      </div>
+    </div>
+  )
+}
