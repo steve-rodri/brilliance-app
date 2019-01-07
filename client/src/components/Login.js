91ebfdc60e5e5a3dced5d0aa27cf36a7d4f69e46
Steve <steve.rodri@gmail.com>
2019-01-07 12:42:51 -0500

    Create Login Form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login'
+import logo_t from './images/logo_t.GIF'
 import './Login.css'
 
-export default function Login (props) {
+export default function Login(props) {
   return (
-    <div>
-      <div>
-        <img />
-      </div>
-      <div>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          scope="https://www.googleapis.com/auth/calendar"
-          buttonText="Login with Google"
-          onSuccess={props.responseGoogle}
-          onFailure={props.responseGoogle}
-        />
+    <div className="login-page">
+      <div className="login-form">
+        <div className="logo-container">
+          <img id="logo" src={logo_t} alt='logo'/>
+        </div>
+        <div className='google-login-container'>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            scope="https://www.googleapis.com/auth/calendar"
+            buttonText="Login with Google"
+            onSuccess={props.responseGoogle}
+            onFailure={props.responseGoogle}
+          />
+        </div>
       </div>
     </div>
   )
