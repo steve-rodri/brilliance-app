69a312b6f1417c2c776b9d6236ebc41cf6e109b3
Steve <steve.rodri@gmail.com>
2019-01-07 15:52:28 -0500

    Add Dashboard Component

---
+++ client/src/components/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Dashboard.css'
+import Header from '../Header/index.js'
+
+export default function Dashboard(){
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
