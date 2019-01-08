8396e0e39ac730acbf607e3f4514d911434acd0f
Steve <steve.rodri@gmail.com>
2019-01-08 12:34:19 -0500

    Update Login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -11,53 +11,49 @@ export default class Login extends Component {
       redirectToDashboard: false
     }
   }
-
   componentDidMount(){
-    if (this.loggedIn()) {
+    const token = localStorage.getItem('google_access_token');
+    if (token) {
       this.setState({
         redirectToDashboard: true
       })
     }
   }
 
-  loggedIn = () => {
-    const token = localStorage.getItem('google_access_token')
-    if (token) {
-      return true
-    } else {
-      return false
-    }
-  }
-
   responseGoogle = (resp) => {
-    this.setState({
-      redirectToDashboard:true
-    })
     if (resp.accessToken) {
       localStorage.setItem('google_access_token',resp.accessToken)
       localStorage.setItem('profileObj', JSON.stringify(resp.profileObj))
     }
+    const token = localStorage.getItem('google_access_token');
+    if (token) {
+      this.setState({
+        redirectToDashboard: true
+      })
+    }
   }
 
   render(){
-    if (this.state.redirectToDashboard) return (<Redirect to="/admin" />)
-    return (
-      <div className="login-page">
-        <div className="login-form">
-          <div className="logo-container">
-            <img className="logo" src={logo_t} alt='logo'/>
-          </div>
-          <div className='google-login-container'>
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              scope="https://www.googleapis.com/auth/calendar"
-              buttonText="Login with Google"
-              onSuccess={this.responseGoogle}
-              onFailure={this.responseGoogle}
-            />
+    if (this.state.redirectToDashboard) {
+      return (<Redirect to="/admin" />)
+    } else {
+      return (
+        <div className="login-page">
+          <div className="login-form">
+            <div className="logo-container">
+              <img className="logo" src={logo_t} alt='logo'/>
+            </div>
+            <div className='google-login-container'>
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                scope="https://www.googleapis.com/auth/calendar"
+                buttonText="Login with Google"
+                onSuccess={this.responseGoogle}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
