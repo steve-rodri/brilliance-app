92c89507fefd59a5b5fefaeb1c91120cab3d82cb
Steve <steve.rodri@gmail.com>
2019-01-08 12:59:07 -0500

    Update Invoices Component to fetch all invoices

--- client/src/components/Invoices/index.js
+++ client/src/components/Invoices/index.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import Header from '../Header/index.js'
 import ListPage from '../ListPage/index.js'
+import { invoice } from '../../services/invoice'
 
 export default class Invoices extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      invoices: []
     }
+  }
+
+  componentDidMount(){
+    await this.fetchAllInvoices()
+  }
+
+  fetchAllInvoices(){
+    const invoices = await invoice.getAll();
+    this.setState({
+      invoices
+    })
   }
 
   render(){
     return (
       <div className="ListPage">
         <Header removeUser={this.props.removeUser}/>
-        <ListPage title="Invoices" categories={['All', 'Production', 'CANS', 'THC', 'CATP']}/>
+        <ListPage
+          title="Invoices"
+          categories={['All', 'Production', 'CANS', 'THC', 'CATP']}
+          subtitles={['client & date', 'type', 'status', 'balance']}
+          invoices={this.state.invoices}
+        />
       </div>
     )
   }
