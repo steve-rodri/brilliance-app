51e04cfd241875f265e4d7aaa7ab727baa068c04
Steve <steve.rodri@gmail.com>
2019-01-08 12:58:54 -0500

    Update Clients Component to fetch all clients

--- client/src/components/Clients/index.js
+++ client/src/components/Clients/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../Header/index.js'
 import ListPage from '../ListPage/index.js'
+import { client } from '../../services/client'
 
 export default class Clients extends Component {
   constructor(props){
@@ -10,11 +11,27 @@ export default class Clients extends Component {
     }
   }
 
+  componentDidMount(){
+    await this.fetchaAllClients()
+  }
+
+  fetchAllClients = async() => {
+    const cliets = await client.getAll();
+    this.setState({
+      clients
+    })
+  }
+
   render(){
     return (
       <div className="ListPage">
         <Header removeUser={this.props.removeUser}/>
-        <ListPage title="Clients" categories={['All', 'Production', 'CANS', 'THC', 'CATP']}/>
+        <ListPage
+          title="Clients"
+          categories={['All', 'Production', 'CANS', 'THC', 'CATP']}
+          subtitles={['name / company', 'contact info', 'next event', 'balance']}
+          invoices={this.state.clients}
+        />
       </div>
     )
   }
