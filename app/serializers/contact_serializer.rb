e993c0d10aee8bb792be8fd2a6fc7a948224cb54
Steve <steve.rodri@gmail.com>
2019-01-09 15:42:41 -0500

    Add Full Name to Contact Serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,8 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id,
   :photo,
   :prefix,
+  :full_name,
   :first_name,
   :last_name,
   :phone_number,
   :work_email
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
