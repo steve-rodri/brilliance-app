e2b965adb88418970251cc532208cf095387f5b8
Steve <steve.rodri@gmail.com>
2019-01-09 21:46:29 -0500

    Create EventDetail with sub components to render with tab control

---
+++ client/src/components/EventDetail/CashFlow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CashFlow extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Cash Flow</h1>
+      </div>
+    )
+  }
+}
