265533130159ee80d706504175402a4ae0ceb4ca
Steve <steve.rodri@gmail.com>
2019-01-11 13:03:30 -0500

    Add method to create custom SQL queries to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def execute_sql(sql)
+    results = ActiveRecord::Base.connection.execute(sql)
+    if results.present?
+        return results
+    else
+        return nil
+    end
+  end
 end
