18fbea823d8c99ed63c7021bd4174dbd5d5d6aab
Steve <steve.rodri@gmail.com>
2019-01-13 10:48:30 -0500

    Add Invoice Detail

---
+++ client/src/components/Invoices/InvoiceDetail/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class InvoiceDetail extends Component {
+  render(){
+    return (
+      <div>
+      <h1>Invoice Detail</h1>
+      </div>
+    )
+  }
+}
--- client/src/components/Invoices/index.js
+++ client/src/components/Invoices/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import Header from '../Header/index.js'
 import ListPage from '../ListPage/index.js'
+import InvoiceDetail from './InvoiceDetail/index.js'
 import { invoice } from '../../services/invoice'
 
 export default class Invoices extends Component {
@@ -22,16 +24,35 @@ export default class Invoices extends Component {
     })
   }
 
+  List = () => {
+    const { invoices } = this.state
+    return (
+      <ListPage
+        title="Invoices"
+        categories={['All', 'Production', 'CANS', 'THC', 'CATP']}
+        subtitles={['client & date', 'type', 'status', 'balance']}
+        data={invoices}
+      />
+    )
+  }
+
+  Show = ({ match }) => {
+    const req_id = parseInt(match.params.id)
+    const invoices = this.state.invoices
+    const invoice = invoices.find(invoice => invoice.id === req_id)
+    return (
+      <InvoiceDetail invoice={invoice} invoiceId={req_id}/>
+    )
+  }
+
   render(){
+    const { location, match } = this.props
     return (
       <div className="ListPage">
-        <Header removeUser={this.props.removeUser}/>
-        <ListPage
-          title="Invoices"
-          categories={['All', 'Production', 'CANS', 'THC', 'CATP']}
-          subtitles={['client & date', 'type', 'status', 'balance']}
-          data={this.state.invoices}
-        />
+        <Header location={location}/>
+
+        <Route exact path={match.path} render={this.List}/>
+        <Route exact path={`${match.path}/:id`} render={this.Show}/>
       </div>
     )
   }
