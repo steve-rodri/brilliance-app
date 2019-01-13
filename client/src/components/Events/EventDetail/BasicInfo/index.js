f23e1ab6a044fd9c95aabfcd0e2bc9269c366acf
Steve <steve.rodri@gmail.com>
2019-01-12 20:19:14 -0500

    Can Update Event

--- client/src/components/Events/EventDetail/BasicInfo/index.js
+++ client/src/components/Events/EventDetail/BasicInfo/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import MainInfo from './MainInfo/index.js'
-import moment from 'moment'
-import { event } from '../../../../services/event'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPencilAlt, faCheck } from '@fortawesome/free-solid-svg-icons'
@@ -11,16 +9,8 @@ library.add(faPencilAlt)
 library.add(faCheck)
 
 export default class BasicInfo extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      editMode: false
-    }
-  }
-
   render(){
-    const { editMode } = this.state
-    const { event, searchFieldData, fields } = this.props
+    const { event, fields, searchFieldData, editMode } = this.props
     return (
       <div className="BasicInfo--container">
         <div className="BasicInfo--header">
@@ -28,14 +18,14 @@ export default class BasicInfo extends Component {
           {editMode?
             <div className="BasicInfo--editButton" onClick={this.props.handleSubmit}><FontAwesomeIcon icon="check" size="2x"/></div>
             :
-            <div className="BasicInfo--editButton" onClick={this.edit}><FontAwesomeIcon icon="pencil-alt" size="2x"/></div>}
+            <div className="BasicInfo--editButton" onClick={this.props.edit}><FontAwesomeIcon icon="pencil-alt" size="2x"/></div>}
         </div>
 
         <MainInfo
           event={event}
           searchFieldData={searchFieldData}
           fields={fields}
-          editMode={this.state.editMode}
+          editMode={this.props.editMode}
           handleSelect={this.props.handleSelect}
           handleChange={this.props.handleChange}
           handleSearchChange={this.props.handleSearchChange}
