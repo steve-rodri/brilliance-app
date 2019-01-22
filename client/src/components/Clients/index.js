7a39ef1f56acde790992c9b368a29ac1e3ae4643
Steve <steve.rodri@gmail.com>
2019-01-22 18:05:46 -0500

    Add Category control to Clients

--- client/src/components/Clients/index.js
+++ client/src/components/Clients/index.js
@@ -8,22 +8,23 @@ export default class Clients extends Component {
     super(props)
     this.state = {
       clients: [],
+      category: 'All',
       hasMore: true
     }
   }
 
   fetchClients = async(page) => {
     const loadedClients = await client.getAll(page);
-    this.updateClients(loadedClients)
+    this.updateClients(loadedClients, "All")
   }
 
   fetchClientsByCategory = async(category) => {
     const loadedClients = await client.findByCategory(category);
     this.resetClients()
-    this.updateClients(loadedClients)
+    this.updateClients(loadedClients, category)
   }
 
-  updateClients = (clients) => {
+  updateClients = (clients, category) => {
     if (clients) {
       const clts = [...this.state.clients]
       clients.forEach(c => clts.push(c))
@@ -32,33 +33,42 @@ export default class Clients extends Component {
 
         this.setState({
           clients,
+          category: category,
           hasMore: false
         })
       } else {
-        this.setState({ clients })
+        this.setState({
+          clients,
+          category: category
+        })
       }
     } else {
-      this.resetClients()
+      this.setState({
+        clients: [],
+        category: category
+      })
     }
   }
 
   resetClients = () => {
-    this.setState({clients: []})
+    this.setState({ clients: [] })
   }
 
   render(){
     const { location } = this.props
-    const { clients, hasMore } = this.state
+    const { clients, category, hasMore } = this.state
     return (
       <div className="Section">
         <Header location={location} />
         <ListPage
           title="Clients"
-          categories={['All', 'Production', 'CANS', 'THC', 'CATP']}
+          category={category}
+          categories={['Production', 'CANS', 'THC', 'CATP']}
           subtitles={['name / company', 'contact info', 'next event', 'balance']}
           data={clients}
           load={this.fetchClients}
           hasMore={hasMore}
+          fetchAllClients={this.fetchClients}
           fetchByCategory={this.fetchClientsByCategory}
         />
       </div>
