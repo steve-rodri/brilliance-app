83a3d2d4a78ec17ee3772d0f18c5320c2ab798bd
Steve <steve.rodri@gmail.com>
2019-02-04 18:26:52 -0500

    Add Modal to Client List Page

--- client/src/components/ListPage/index.js
+++ client/src/components/ListPage/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal';
 import { Redirect } from 'react-router-dom'
 import List from '../List/index.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,19 +8,37 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './ListPage.css'
 
 library.add(faPlus)
+Modal.setAppElement('#root');
 
 export default class ListPage extends Component {
   constructor(props){
     super(props)
     this.state = {
-      redirectToCreateNew: false
+      redirectToCreateNew: false,
+      showModal: false
     }
   }
 
-  createNew = () => {
-    this.setState({
-      redirectToCreateNew: true
-    })
+  handleOpenModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
+
+  createNew = (title) => {
+    switch (title) {
+      case "Clients":
+        this.handleOpenModal();
+        break;
+      default:
+        this.setState({
+          redirectToCreateNew: true
+        })
+        break;
+    }
   }
 
   styleActiveMenu = (category) => {
@@ -79,7 +98,7 @@ export default class ListPage extends Component {
           {title === "Events" || title === "Clients"?
             <div
               className="ListPage--button create"
-              onClick={this.createNew}
+              onClick={() => this.createNew(title)}
             >
               <span className="button-text">Add New</span>
               {<FontAwesomeIcon className="button-icon" icon="plus" size="2x"/>}
@@ -103,6 +122,16 @@ export default class ListPage extends Component {
           />
 
         </main>
+
+        <Modal
+          isOpen={this.state.showModal}
+          onRequestClose={this.handleCloseModal}
+          className="ListPage--modal"
+          overlayClassName="ListPage--modal-overlay"
+        >
+
+        </Modal>
+
       </div>
     )
   }
