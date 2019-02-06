d6082b386fd9aedfebfe2d8934ffe1b37fce9902
Steve <steve.rodri@gmail.com>
2019-02-05 21:02:15 -0500

    Add route on backend to display client events

--- client/src/services/client.js
+++ client/src/services/client.js
@@ -9,6 +9,14 @@ const client = {
       console.log(e)
     }
   },
+  findById: async function (id) {
+    try {
+      const resp = await axios.get(`/api/clients/${id}`)
+      return resp.data
+    } catch (e) {
+      console.log(e)
+    }
+  },
   findByCategory: async function (category){
     try {
       const resp = await axios.get(`/api/clients?category=${category}`)
@@ -20,6 +28,14 @@ const client = {
   find: async function (query){
     try {
       const resp = await axios.get(`/api/clients/find?q=${query}`)
+      return resp.data
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  getEvents: async function (page, id){
+    try {
+      const resp = await axios.get(`/api/clients/${id}/events?page=${page}`)
       return resp.data
     } catch (e) {
       console.log(e)
