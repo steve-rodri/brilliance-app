d05ab05f9f1f6b85d67f1c7ed2ddd9ed8071592a
Steve <steve.rodri@gmail.com>
2019-02-05 21:04:14 -0500

    Rename clientName.js to clientHelpers.js

---
+++ client/src/components/Helpers/clientHelpers.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function clientName(client, oneLine){
+  if (client) {
+    if (!client.contactInfo) {
+      if (!client.company) {
+        return null
+      } else {
+        return client.company.name
+      }
+    } else {
+      if (!client.company) {
+        return client.contactInfo.fullName
+      } else if (oneLine) {
+        return `${client.contactInfo.fullName} : ${client.company.name}`
+      } else {
+        return (
+          <div className="client">
+            <p>{client.contactInfo.fullName}</p>
+            <h6>c: {client.company.name}</h6>
+          </div>
+        )
+      }
+    }
+  }
+}
+
+function clientInfo(client){
+  if (client) {
+    if (!client.contactInfo) {
+      if (!client.company) {
+        return null
+      } else {
+        return {
+          name: client.company.name,
+          logo: client.company.logo,
+          phoneNumber: client.company.phoneNumber,
+          website: client.company.website,
+          email: client.company.emailAddresses
+        }
+      }
+    } else {
+      if (!client.company) {
+        return {
+          name: client.contactInfo.fullName,
+          phoneNumber: client.contactInfo.phoneNumber,
+          photo: client.contactInfo.photo,
+          emailAddresses: client.contactInfo.emailAddresses
+        }
+      } else {
+        return {
+          name: client.contactInfo.fullName,
+          phoneNumber: client.contactInfo.phoneNumber,
+          companyNumber: client.company.phoneNumber,
+          emailAddresses: client.contactInfo.emailAddresses,
+          logo: client.company.logo
+        }
+      }
+    }
+  }
+}
+
+
+
+export {
+  clientName,
+  clientInfo
+}
