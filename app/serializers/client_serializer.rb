10a2135e43b66417be3894447dfa460ef6522ad7
Steve <steve.rodri@gmail.com>
2019-02-21 11:51:22 -0500

    fix bug in client controller and serializer where findById was incorrect

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,5 +1,5 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :sorting_name
+  attributes :id
   has_many :event, key: 'events', serializer: NestedEventSerializer
   has_one :contact, key: 'contact_info', serializer: NestedContactSerializer
   has_one :company, serializer: NestedCompanySerializer
