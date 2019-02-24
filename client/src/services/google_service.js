2224b47d557e141eeca161f84f0100d38f6e6fbe
Steve <steve.rodri@gmail.com>
2019-02-24 01:36:07 -0500

    make google_service return one object with methods and update schedule

--- client/src/services/google_service.js
+++ client/src/services/google_service.js
@@ -1,84 +1,132 @@
 import axios from 'axios';
 
-async function getUser(){
-  const accessToken = localStorage.getItem('google_access_token');
-  try {
-      await axios({
-      method: 'get',
-      url: 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-  } catch (e) {
-    localStorage.clear()
-    return null
-  }
-}
+const GOOGLE = {
+  getUser: async function(){
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+        await axios({
+        method: 'get',
+        url: 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    } catch (e) {
+      localStorage.clear()
+      return null
+    }
+  },
 
-async function getGoogleCalendars(){
-  const accessToken = localStorage.getItem('google_access_token');
-  try {
-    const resp = await axios({
-      method: 'get',
-      url: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-    return resp.data.items
-  } catch (e) {
-    localStorage.clear()
-    return null
-  }
-}
+  getCalendars: async function (){
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+      const resp = await axios({
+        method: 'get',
+        url: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      return resp.data.items
+    } catch (e) {
+      localStorage.clear()
+      return null
+    }
+  },
 
-async function getGoogleEvents(calendar_id){
-  const accessToken = localStorage.getItem('google_access_token');
-  try {
-    const resp = await axios({
-      method: 'get',
-      url:`https://www.googleapis.com/calendar/v3/calendars/${calendar_id}/events?alwaysIncludeEmail=true&orderby=starttime&maxResults=2500`,
-      headers:{
-        Authorization: `Bearer ${accessToken}`
-      },
-    })
-    return resp.data.items
-  } catch (e) {
-    localStorage.clear()
-    return null
-  }
-}
-
-async function createGoogleEvent(calendar_id, start, end, summary){
-  const accessToken = localStorage.getItem('google_access_token');
-  try {
-    const resp = await axios({
-      method: 'post',
-      url:`https://www.googleapis.com/calendar/v3/calendars/${calendar_id}/events`,
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      },
-      data: {
-        start: {
-          dateTime: start
+  getEvents: async function(calendar_id){
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+      const resp = await axios({
+        method: 'get',
+        url:`https://www.googleapis.com/calendar/v3/calendars/${calendar_id}/events?alwaysIncludeEmail=true&orderby=starttime&maxResults=2500`,
+        headers:{
+          Authorization: `Bearer ${accessToken}`
         },
-        end: {
-          dateTime: end
+      })
+      return resp.data.items
+    } catch (e) {
+      localStorage.clear()
+      return null
+    }
+  },
+
+  createEvent: async function(calendarId, data){
+    //required data properties
+    // start : { dateTime }
+    // end : { dateTime }
+    //optional data properties
+    // description
+    // location
+    // summary
+    // transparency
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+      const resp = await axios({
+        method: 'post',
+        url:`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
         },
-        summary: summary
-      }
-    })
-    console.log(resp);
-  } catch (e) {
-    localStorage.clear();
-    return null
+        data: data
+      })
+      return resp.data;
+    } catch (e) {
+      console.log(e)
+    }
+  },
+
+  patchEvent: async function(calendarId, eventId, data){
+    //optional data properties
+    // start : { dateTime }
+    // end : { dateTime }
+    // description
+    // location
+    // summary
+    // transparency
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+      const resp = await axios({
+        method: 'patch',
+        url:`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}?sendUpdates='none'`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+        data: data
+      })
+      return resp.data;
+    } catch (e) {
+      console.log(e)
+    }
+  },
+
+  importEvents: async function(calendarId, data){
+    //required data properties
+    // iCalUID
+    // start : { dateTime }
+    // end : { dateTime }
+    //optional data properties
+    // description
+    // location
+    // summary
+    // transparency
+    const accessToken = localStorage.getItem('google_access_token');
+    try {
+      const resp = await axios({
+        method: 'post',
+        url:`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/import`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+        data: data
+      })
+      return resp.data;
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
 export {
-  getUser,
-  getGoogleCalendars,
-  getGoogleEvents,
-  createGoogleEvent
+  GOOGLE
 }
