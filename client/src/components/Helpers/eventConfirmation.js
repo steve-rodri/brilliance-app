a0c10da40091b5c74726869a3c963cec3f30ffb5
Steve <steve.rodri@gmail.com>
2019-02-26 16:09:00 -0500

    small style changes

--- client/src/components/Helpers/eventConfirmation.js
+++ client/src/components/Helpers/eventConfirmation.js
@@ -3,26 +3,23 @@ function styleConfirmation(msg){
     case "Unconfirmed":
       return {
         border: '2px solid rgba(210,210,0,1)',
-        borderRadius: '5px',
         color: 'rgba(210,180,0,1)',
         backgroundColor: 'inherit',
-        padding: '5px 15px',
+        padding: '5px 15px'
       }
     case "Confirmed":
       return {
         backgroundColor: 'limegreen',
         fontWeight: 'bold',
         padding: '5px 15px',
-        color: 'white',
-        borderRadius: '5px',
+        color: 'white'
       }
     case "Cancelled":
       return {
-        backgroundColor: 'inherit',
+        backgroundColor: 'darkred',
+        fontWeight: 'bold',
         padding: '5px 15px',
-        color: 'darkred',
-        border: '2px solid darkred',
-        borderRadius: '5px',
+        color: 'white'
       }
     default:
       return {}
