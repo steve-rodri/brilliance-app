6d810f2d4626c150b56cefc85afe8120ae123dac
Steve <steve.rodri@gmail.com>
2019-02-26 11:49:02 -0500

    add migration - add creator to events through contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   has_one :client, dependent: :nullify
   has_many :email_address, dependent: :destroy
+  has_many :event_creators, class_name: 'Event', foreign_key: 'creator', dependent: :nullify
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,5 @@ class Event < ApplicationRecord
   has_one :invoice, dependent: :nullify
   belongs_to :place_location, class_name: 'Place', foreign_key: 'location_id', optional: true
   belongs_to :place_call_location, class_name: 'Place', foreign_key: 'call_location_id', optional: true
-  belongs_to :organizer, class_name: 'EmailAddress', foreign_key: 'organizer', optional: true
-  belongs_to :creator, class_name: 'EmailAddress', foreign_key: 'creator', optional: true
+  belongs_to :creator, class_name: 'Contact', foreign_key: 'creator', optional: true
 end
