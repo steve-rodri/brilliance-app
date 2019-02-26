bf7fd4f69b3ddecbd51ad04ad313717fdd1abf36
Steve <steve.rodri@gmail.com>
2019-02-26 16:01:20 -0500

    Update client list item

--- client/src/components/ListItem/Client.js
+++ client/src/components/ListItem/Client.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import './Client.css'
 
 export default function Client(props) {
-  const { item, type, numColumns, displayColumn, styleItem } = props
+  const { item, type, displayColumn } = props
   const client = item.contactInfo
   const company = item.company
 
   return (
     <Link to={{pathname: `/admin/${type.toLowerCase()}/${item.id}`, state: { modal: true } }} style={{textDecoration: 'none', color: 'black'}}>
-      <div className="List-Item" style={styleItem(item, type, numColumns)}>
+      <div className="List-Item" style={style()}>
 
-        <div style={displayColumn('name / company')}>
+        <div className="Client--name" style={displayColumn('name / company')}>
           <h3>{client && client.fullName}</h3>
           <h4>{company && company.name}</h4>
           {client && <p>{client.phoneNumber && `${client.phoneNumber}`}</p>}
           {client && client.emailAddresses && client.emailAddresses.length > 0 && <a href={`mailto:${client.emailAddresses[0].address}`}>{client.emailAddresses[0].address}</a>}
         </div>
 
-        <div style={displayColumn('next event')} className="List-Item--next-event">
+        <div className="Client--event" style={displayColumn('next event')}>
           {latestEvent(client)}
         </div>
+
+        <div className="Client--balance" style={displayColumn('balance')}>$0.00</div>
 
       </div>
     </Link>
   )
+
+  function style(){
+    return {
+      gridTemplateColumns: '1fr 2fr 1fr'
+    }
+  }
 
   function latestEvent(){
     if (item) {
