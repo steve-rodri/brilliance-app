a5e0b1663bbe534f9c8e856573f6b660c822b4bd
Steve <steve.rodri@gmail.com>
2019-02-27 12:58:06 -0500

    update event and contact serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,8 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :action, :break, :break_start, :client, :call_time, :clock_out, :confirmation, :creator, :description, :driving_time, :end, :gc_id, :html_link, :kind, :notes, :start, :summary, :tags
+  attributes :id, :action, :break, :break_start, :client, :call_time, :clock_out, :confirmation, :creator, :description, :driving_time, :end, :gc_id, :html_link, :i_cal_UID, :kind, :notes, :start, :summary, :tags
   belongs_to :client, foreign_key: true, serializer: NestedClientSerializer
   has_one :invoice, serializer: NestedInvoiceSerializer
   has_one :place_location, serializer: NestedPlaceLocationSerializer
   has_one :place_call_location, serializer: NestedPlaceCallLocationSerializer
   has_one :creator, serializer: ContactSerializer
+  has_many :event_employees, key:'staff', serializer: NestedEventEmployeeSerializer
 end
