6206b28fc3a24575cfe87552e6ab1013b960b180
Steve <steve.rodri@gmail.com>
2019-03-01 09:55:12 -0500

    Add synchronize events method to fetch events from google and update db

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { GOOGLE } from '../../services/google_service'
+import { event } from '../../services/event'
+import { formatFromGoogle } from '../Helpers/googleFormatters'
 import Admin from './Admin'
 
 export default class Main extends Component {
   constructor(props){
     super(props)
     this.state = {}
+  }
+
+  async componentDidMount(){
+    //await this.synchronizeEvents()
   }
 
   user = () => {
@@ -17,9 +24,22 @@ export default class Main extends Component {
     return user
   }
 
+  synchronizeEvents = async() => {
+    const user = this.user()
+    if (user) {
+      const calendars = await GOOGLE.getCalendars();
+      if (calendars) {
+        const jobsCalendar = calendars.find(calendar => calendar.summary = 'Jobs' && calendar.id.includes('bob@brilliancepro.com'))
+        const events = await GOOGLE.getEvents(jobsCalendar.id)
+        const evts = await Promise.all(events.map(async evt => {return await formatFromGoogle(evt)}))
+        await Promise.all(evts.map(async evt => await event.sync(evt)))
+      }
+    }
+  }
+
   render(){
     const token = localStorage.getItem('google_access_token')
-    if (!token ) return (<Redirect to="/login"/>)
+    if (!token) return (<Redirect to="/login"/>)
     return(
       <div className="App">
         <Switch>
