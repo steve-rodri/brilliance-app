8175e7f0b9c3ff38ed0723415ea766955e244a53
Steve <steve.rodri@gmail.com>
2019-03-04 14:28:39 -0500

    Update Client list item so it won't display columns on mobile

--- client/src/components/ListItem/Client.js
+++ client/src/components/ListItem/Client.js
@@ -4,28 +4,44 @@ import moment from 'moment'
 import './Client.css'
 
 export default function Client(props) {
-  const { item, type, displayColumn } = props
+  const { item, type, numColumns, displayColumn } = props
   const client = item.contactInfo
   const company = item.company
 
   return (
     <Link to={{pathname: `/admin/${type.toLowerCase()}/${item.id}`, state: { modal: true } }} style={{textDecoration: 'none', color: 'black'}}>
-      <div className="List-Item" style={style()}>
+      {
+        numColumns?
+        <div className="List-Item" style={style()}>
 
-        <div className="Client--name" style={displayColumn('name / company')}>
-          <h3>{client && client.fullName}</h3>
-          <h4>{company && company.name}</h4>
-          {client && <p>{client.phoneNumber && `${client.phoneNumber}`}</p>}
-          {client && client.emailAddresses && client.emailAddresses.length > 0 && <a href={`mailto:${client.emailAddresses[0].address}`}>{client.emailAddresses[0].address}</a>}
+          <div className="Client--name" style={displayColumn('name / company')}>
+            <h3>{client && client.fullName}</h3>
+            <h4>{company && company.name}</h4>
+            {client && <p>{client.phoneNumber && `${client.phoneNumber}`}</p>}
+            {client && client.emailAddresses && client.emailAddresses.length > 0 && <a href={`mailto:${client.emailAddresses[0].address}`}>{client.emailAddresses[0].address}</a>}
+          </div>
+
+          <div className="Client--event" style={displayColumn('next event')}>
+            {latestEvent(client)}
+          </div>
+
+          <div className="Client--balance" style={displayColumn('balance')}>$0.00</div>
+
         </div>
+        :
+        <div className="List-Item" style={style()}>
 
-        <div className="Client--event" style={displayColumn('next event')}>
-          {latestEvent(client)}
+          <div className="Client--name">
+            <h3>{client && client.fullName}</h3>
+            <h4>{company && company.name}</h4>
+            {client && <p>{client.phoneNumber && `${client.phoneNumber}`}</p>}
+            {client && client.emailAddresses && client.emailAddresses.length > 0 && <a href={`mailto:${client.emailAddresses[0].address}`}>{client.emailAddresses[0].address}</a>}
+          </div>
+
+          <div className="Client--balance">$0.00</div>
+
         </div>
-
-        <div className="Client--balance" style={displayColumn('balance')}>$0.00</div>
-
-      </div>
+      }
     </Link>
   )
 
