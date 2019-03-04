ca2e01d0d7377cba6ea7947720f4c4c2c2ce9f7b
Steve <steve.rodri@gmail.com>
2019-03-04 14:21:12 -0500

    update event employee migration default values

--- db/migrate/20190227170306_create_event_employees.rb
+++ db/migrate/20190227170306_create_event_employees.rb
@@ -1,15 +1,15 @@
 class CreateEventEmployees < ActiveRecord::Migration[5.2]
   def change
     create_table :event_employees do |t|
-      t.string :confirmation
-      t.boolean :paid?
+      t.string :confirmation, default: 'needsAction'
+      t.boolean :paid?, default: false
       t.string :position
-      t.float :rate
+      t.float :rate, default: 0
       t.date :clock_in
       t.date :clock_out
-      t.integer :break_minutes
-      t.boolean :break?
-      t.boolean :hourly?
+      t.integer :break_minutes, default: 0
+      t.boolean :break?, default: false
+      t.boolean :hourly?, default: false
       t.references :employee, foreign_key: true
       t.references :event, foreign_key: true
 
