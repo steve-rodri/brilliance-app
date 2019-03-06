b1b00a33b47982b79f8de6364e9b94874735f1d5
Steve <steve.rodri@gmail.com>
2019-03-06 14:46:11 -0500

    Create StaffModal

---
+++ client/src/components/Main/Views/Events/EventDetail/StaffModal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import StaffSelector from './BasicInfo/Staff/StaffSelector'
+import { closeIcon } from '../../../../Helpers/icons'
+import './StaffModal.css'
+
+export default class StaffModal extends Component {
+  render(){
+    const { close } = this.props
+    return (
+      <div className="StaffModal--overlay" onClick={close}>
+        <div className="StaffModal">
+          <div className="StaffModal--content">
+            <div onClick={close} className="StaffModal--close">{closeIcon('2x', 'gray')}</div>
+            <StaffSelector {...this.props}/>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
