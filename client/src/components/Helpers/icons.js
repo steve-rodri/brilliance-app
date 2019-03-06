ccd37faf54a6154eb93dd8e1f125ddaa06e2b893
Steve <steve.rodri@gmail.com>
2019-03-06 14:47:26 -0500

    update icons

--- client/src/components/Helpers/icons.js
+++ client/src/components/Helpers/icons.js
@@ -28,39 +28,39 @@ library.add(faTimes)
 function statusIcon(str, size){
   switch (str) {
     case 'needsAction':
-      return <FontAwesomeIcon className="Icon" icon="circle" size={size}/>
+      return <FontAwesomeIcon  className="Icon" icon="circle" size={size}/>
     case 'accepted':
-      return <FontAwesomeIcon className="Icon" color="limegreen" icon="check-circle" size={size}/>
+      return <FontAwesomeIcon  className="Icon" color="limegreen" icon="check-circle" size={size}/>
     case 'tentative':
-      return <FontAwesomeIcon className="Icon" color="gold" icon="question-circle" size={size}/>
+      return <FontAwesomeIcon  className="Icon" color="gold" icon="question-circle" size={size}/>
     case 'declined':
-      return <FontAwesomeIcon className="Icon" color="red" icon="times-circle" size={size}/>
+      return <FontAwesomeIcon  className="Icon" color="red" icon="times-circle" size={size}/>
     default:
   }
 }
 
 function addIcon(size){
-  return <FontAwesomeIcon className="Icon" color="limegreen" icon="plus-circle" size={size}/>
+  return <FontAwesomeIcon  className="Icon" color="limegreen" icon="plus-circle" size={size}/>
 }
 
 function deleteIcon(size){
-  return <FontAwesomeIcon className="Icon" color="red" icon="minus-circle" size={size}/>
+  return <FontAwesomeIcon  className="Icon" color="red" icon="minus-circle" size={size}/>
 }
 
-function closeIcon(size, className, color){
-  return <FontAwesomeIcon className={`Icon ${className}`} color="gray" icon="times" size={size}/>
+function closeIcon(size, color, className){
+  return <FontAwesomeIcon  className={`Icon ${className}`} color={color} icon="times" size={size}/>
 }
 
 function pencil(size, className, color){
-  return <FontAwesomeIcon className={`Icon ${className}`} icon="pencil-alt" color={color? color : null} size={size}/>
+  return <FontAwesomeIcon  className={`Icon ${className}`} icon="pencil-alt" color={color? color : null} size={size}/>
 }
 
 function trash(size, className, color){
-  return <FontAwesomeIcon className={`Icon ${className}`} icon="trash" color={color? color : null} size={size}/>
+  return <FontAwesomeIcon  className={`Icon ${className}`} icon="trash" color={color? color : null} size={size}/>
 }
 
 function check(size, className, color){
-  return <FontAwesomeIcon className={`Icon ${className}`} icon="check" color={color? color : null} size={size? size : '1x'}/>
+  return <FontAwesomeIcon  className={`Icon ${className}`} icon="check" color={color? color : null} size={size? size : '1x'}/>
 }
 
 export {
