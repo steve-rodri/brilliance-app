e7d29ee75e1ef4a54025481e7344a10c30e741dc
Steve <steve.rodri@gmail.com>
2019-04-13 14:50:00 -0400

    change full_name method in contact serializer to use prefix

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,25 +1,35 @@
-class ContactSerializer < ActiveModel::Serializer
+class ContactSerializer < ApplicationSerializer
   attributes :id,
   :photo,
   :prefix,
   :full_name,
   :first_name,
   :last_name,
-  :phone_number,
-  :work_email
+  :phone_number
 
   has_many :email_address, key: 'email_addresses', serializer: NestedEmailAddressSerializer
-
+  
   def full_name
     if object.first_name
       if object.last_name
-        "#{object.first_name} #{object.last_name}"
+
+        if object.prefix
+          "#{object.prefix} #{object.first_name} #{object.last_name}"
+        else
+          "#{object.first_name} #{object.last_name}"
+        end
+
       else
         "#{object.first_name}"
       end
+
     else
       if object.last_name
-        "#{object.last_name}"
+        if object.prefix
+          "#{object.prefix} #{object.last_name}"
+        else
+          "#{object.last_name}"
+        end
       end
     end
   end
