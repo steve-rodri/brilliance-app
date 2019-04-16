5bd1b9e7ba740e68754a9a66ed08a50fd1e36007
Steve <steve.rodri@gmail.com>
2019-04-16 10:45:33 -0400

    update Items with name field, accepts nested attr for lines and contents

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   has_many :lines, dependent: :nullify
   has_many :item_contents, dependent: :destroy
   has_many :contents, through: :item_contents
+  accepts_nested_attributes_for :lines, :item_contents, :contents
 end
--- db/migrate/20190315201946_create_items.rb
+++ db/migrate/20190315201946_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string :name
       t.string :kind
       t.string :install
       t.string :description
