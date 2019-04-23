0a19b7fc89ee50543e5b18ec799d83ba55fd7f84
Steve <steve.rodri@gmail.com>
2019-04-23 17:10:42 -0400

    add invoiceStatus helper and refactor invoice list item

---
+++ client/src/helpers/invoiceStatus.js
@@ -0,0 +1,57 @@
+export function styleStatus(paymentStatus, invoiceType) {
+  console.log(paymentStatus)
+  if (invoiceType === 'Proposal') {
+    return { display: 'none'}
+  } else {
+    switch (paymentStatus) {
+      case 'Not Paid':
+        return (
+          {
+            color:'white',
+            backgroundColor:'darkred'
+
+          }
+        )
+      case 'Outstanding Balance':
+        return (
+          {
+            color:'white',
+            backgroundColor:'rgba(210,210,0,1)'
+
+          }
+        )
+      case 'Paid In Full':
+        return (
+          {
+            color:'white',
+            backgroundColor:'limegreen'
+
+          }
+        )
+      case 'Partially Refunded':
+        return (
+          {
+            color:'white',
+            backgroundColor:'indianred'
+
+          }
+        )
+      case 'Fully Refunded':
+        return (
+          {
+            color:'white',
+            backgroundColor:'indianred'
+
+          }
+        )
+      default:
+        return (
+          {
+            color:'white',
+            backgroundColor:'darkred'
+
+          }
+        )
+    }
+  }
+}
