5fa58f6828bba28e1cab22ad647cdc65651978e5
Steve <steve.rodri@gmail.com>
2019-05-02 17:43:04 -0400

    Another attempt to preventPullToRefresh on Mobile

--- client/src/helpers/preventPullToRefresh.js
+++ client/src/helpers/preventPullToRefresh.js
@@ -1,4 +1,4 @@
-export default function preventPullToRefresh() {
+export function preventPullToRefreshs() {
   const element = 'html'
   let prevent = false;
 
@@ -16,3 +16,46 @@ export default function preventPullToRefresh() {
     }
   });
 }
+
+export default function preventPullToRefresh() {
+    let touchStartHandler,
+        touchMoveHandler,
+        touchPoint;
+
+    // Only needed for touch events on chrome.
+    if ((window.chrome || navigator.userAgent.match("CriOS"))
+        && "ontouchstart" in document.documentElement) {
+
+        touchStartHandler = function(e) {
+            // Only need to handle single-touch cases
+            touchPoint = e.touches.length === 1 ? e.touches[0].clientY : null;
+        };
+
+        touchMoveHandler = function(e) {
+            var newTouchPoint;
+
+            // Only need to handle single-touch cases
+            if (e.touches.length !== 1) {
+                touchPoint = null;
+
+                return;
+            }
+
+            // We only need to defaultPrevent when scrolling up
+            newTouchPoint = e.touches[0].clientY;
+            if (newTouchPoint > touchPoint) {
+                e.preventDefault();
+            }
+            touchPoint = newTouchPoint;
+        };
+
+        document.addEventListener("touchstart", touchStartHandler, {
+            passive: false
+        });
+
+        document.addEventListener("touchmove", touchMoveHandler, {
+            passive: false
+        });
+
+    }
+};
