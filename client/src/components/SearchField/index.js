068bbe179b5bb57837ae93e3fd5dd719da614bdf
Steve <steve.rodri@gmail.com>
2019-05-10 16:26:43 -0400

    changes to SearchField - made scrolling more responsive to arrow keys

--- client/src/components/SearchField/index.js
+++ client/src/components/SearchField/index.js
@@ -12,20 +12,27 @@ export default class SearchField extends Component {
     this.searchResults = React.createRef();
   }
 
-  componentWillReceiveProps(nextProps){
-    const { highlightedResult } = this.state;
-    const { searchResults, scroll } = nextProps;
+  componentDidUpdate(prevProps){
+    const { searchResults, scroll, input: { value } } = this.props;
     if (searchResults && searchResults.length) {
-      if (highlightedResult > searchResults.length - 1) {
-        this.setState({ highlightedResult: 0 })
+      if (searchResults !== prevProps.searchResults) {
+        this.scrollToTop()
       }
-      this.setState({ searchResults })
-    } else {
-      this.setState({ searchResults: null })
     }
+    if (prevProps.input.value !== value) {
+      this.setState({ highlightedResult: 0 })
+    }
+    if (scroll && scroll !== prevProps.scroll) {
+      this.setState({ fieldActive: true })
+    }
+  }
 
-    if (scroll) {
-      this.setState({ fieldActive: false })
+  changeHighlightedResult = (e, i) => {
+    e.preventDefault()
+    e.stopPropagation()
+    const { highlightedResult } = this.state
+    if (i !== highlightedResult) {
+      this.setState({ highlightedResult: i })
     }
   }
 
@@ -47,17 +54,17 @@ export default class SearchField extends Component {
       this.setState({
         fieldActive: true
       })
-    // } else {
-    //   this.setState({
-    //     fieldActive: false
-    //   })
+    } else {
+      this.setState({
+        fieldActive: false
+      })
     }
   }
 
   handleCloseResults = (e) => {
-    // this.setState({
-    //   fieldActive: false
-    // })
+    this.setState({
+      fieldActive: false
+    })
   }
 
   choosingResult = (e) => {
@@ -76,20 +83,43 @@ export default class SearchField extends Component {
   }
 
   scrollResults = (direction) => {
+    const scrollPosition = this.searchResults.current.scrollTop
+    const nextUpPosition = Math.floor( scrollPosition / 50 ) * 50
+    const nextDownPosition = Math.ceil( scrollPosition / 50 ) * 50
+
     switch (direction) {
       case 'up':
-        this.searchResults.current.scrollTop -= 228
-        break;
+      if (scrollPosition === 0) break;
+      if (scrollPosition !== nextUpPosition) {
+        this.searchResults.current.scrollTop = nextUpPosition
+      } else {
+        this.searchResults.current.scrollTop -= 150
+      }
+      break;
       case 'down':
-        this.searchResults.current.scrollTop += 228
+      if (scrollPosition === 0) {
+        this.searchResults.current.scrollTop += 150
         break;
+      }
+      if (scrollPosition !== nextDownPosition) {
+        this.searchResults.current.scrollTop = nextDownPosition
+      } else {
+        this.searchResults.current.scrollTop += 150
+      }
+      break;
       default:
-        break;
+      break;
     }
+
   }
 
-  updateHighlightedResult = (direction, list) => {
+  scrollToTop = () => {
+    this.searchResults.current.scrollTop = 0
+  }
+
+  updateHighlightedResult = (direction) => {
     const { highlightedResult } = this.state;
+    const { searchResults: list } = this.props
     switch (direction) {
       case 'up':
         if (highlightedResult > 0) {
@@ -111,23 +141,91 @@ export default class SearchField extends Component {
   }
 
   styleResult = (i) => {
-    const { hoveringResults, highlightedResult } = this.state
-    const style = { padding: '5px'}
-    if (i === highlightedResult && !hoveringResults ) {
+    const { highlightedResult } = this.state
+    const style = { }
+    if (i === highlightedResult) {
       style.backgroundColor = 'var(--light-blue)'
     }
 
     return style
   }
 
+  shouldScrollUp = () => {
+    const { highlightedResult } = this.state
+    const { searchResults } = this.props
+    const scrollPosition = this.searchResults.current.scrollTop
+    const offset = scrollPosition % 150
+    const validOffset = (
+      offset === 0  ||
+      offset === 49 ||
+      offset === 50 ||
+      offset === 99 ||
+      offset === 100
+    )
+    const rem0 = searchResults.length % 3 === 0
+    const rem1 = searchResults.length % 3 === 1
+    const rem2 = searchResults.length % 3 === 2
+
+    const isFirstItem = highlightedResult % 3 === 0
+    const isSecondItem = highlightedResult % 3 === 1
+    const isThirdItem = highlightedResult % 3 === 2
+
+    if (highlightedResult === 0) return false
+
+    if (rem0) {
+      if (isThirdItem) return true
+    }
+    if (rem1) {
+      if (isFirstItem && offset === 0) return true
+      if (isSecondItem && (offset === 50 || offset === 49)) return true
+    }
+    if (rem2) {
+      if (isFirstItem && offset === 0) return true
+      if (isThirdItem && (offset === 100 || offset === 99)) return true
+    }
+
+    if (!validOffset) this.scrollToTop()
+  }
+
+  shouldScrollDown = () => {
+    const { highlightedResult } = this.state
+    const { searchResults } = this.props
+    const scrollPosition = this.searchResults.current.scrollTop
+    const offset = scrollPosition % 150
+
+    const rem0 = searchResults.length % 3 === 0
+    const rem1 = searchResults.length % 3 === 1
+    const rem2 = searchResults.length % 3 === 2
+
+    const isFirstItem = highlightedResult % 3 === 0
+    const isSecondItem = highlightedResult % 3 === 1
+    const isThirdItem = highlightedResult % 3 === 2
+
+    if (highlightedResult === 0) return false
+    if (rem0) {
+      if (isThirdItem) return true
+    }
+    if (rem1) {
+      if (isThirdItem && offset === 0) return true
+      if (isFirstItem && (offset === 50 || offset === 49)) return true
+    }
+    if (rem2) {
+      if (isThirdItem && offset === 0) return true
+      if (isSecondItem && (offset === 100 || offset === 99)) return true
+    }
+  }
+
   render(){
-    const { highlightedResult, hoveringResults, searchResults } = this.state
+    const { highlightedResult, hoveringResults } = this.state
     const {
+      searchResults,
+
       formClassName,
       resultClassName,
       resultsClassName,
       formatResult,
       styleForm,
+
       input:{
         className,
         placeholder,
@@ -135,6 +233,7 @@ export default class SearchField extends Component {
         value,
         tabIndex
       },
+
       handleChange,
       formDataValue,
       onEnter,
@@ -149,7 +248,7 @@ export default class SearchField extends Component {
         onFocus={this.handleFocusSelect}
         onSubmit={(e) => e.preventDefault()}
         onKeyDown={(e) => {
-          if ( (e.key === 'Tab' || e.key === 'Enter') && searchResults ) {
+          if ( e.key === 'Enter' && searchResults ) {
             onEnter(e, name, highlightedResult)
             this.handleCloseResults()
           }
@@ -182,18 +281,18 @@ export default class SearchField extends Component {
             if (e.key === 'ArrowDown'){
 
               e.preventDefault()
-              this.updateHighlightedResult('down', searchResults)
+              this.updateHighlightedResult('down')
 
-              if (highlightedResult !== 0 && (highlightedResult + 1) % 3 === 0) {
+              if (this.shouldScrollDown()) {
                 this.scrollResults('down')
               }
 
             } else if (e.key === 'ArrowUp') {
 
               e.preventDefault()
-              this.updateHighlightedResult('up', searchResults)
+              this.updateHighlightedResult('up')
 
-              if (this.state.highlightedResult !== 0 && (this.state.highlightedResult + 1) % 3 === 0) {
+              if (this.shouldScrollUp()) {
                 this.scrollResults('up')
               }
 
@@ -215,12 +314,13 @@ export default class SearchField extends Component {
               <div
                 style={this.styleResult(i)}
                 key={item.id}
-                className={resultClassName}
+                className={`${resultClassName} SearchField--result`}
                 onClick={(e) => {
                 e.stopPropagation();
                 onSelect(e, name, i)
                 this.handleCloseResults()
                 }}
+                onMouseEnter={(e) => this.changeHighlightedResult(e, i)}
               >
                 <Fragment>{formatResult && formatResult(item)}</Fragment>
               </div>
