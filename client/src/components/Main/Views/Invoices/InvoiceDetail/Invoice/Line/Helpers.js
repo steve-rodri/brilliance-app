e88bc8c4116a7cf02f94db539da3df9dcd204572
Steve <steve.rodri@gmail.com>
2019-05-14 15:09:08 -0400

    refactor line helpers

--- client/src/components/Main/Views/Invoices/InvoiceDetail/Invoice/Line/Helpers.js
+++ client/src/components/Main/Views/Invoices/InvoiceDetail/Invoice/Line/Helpers.js
@@ -4,189 +4,163 @@ import numeral from 'numeral'
 import { ReactComponent as CheckSquare } from '../../../../../../../icons/check-square.svg'
 import { ReactComponent as Square } from '../../../../../../../icons/square.svg'
 
-const description = (item) => {
-  if (item) {
-    const {
-      contents, description, useDescription, useDescriptionOnly
-    } = item
+// -------------Line---------------
 
-    if (useDescriptionOnly) {
-      return description
+export const description = (item) => {
+  if (!item) return
+  const {
+    contents,
+    description,
+    useDescription,
+    useDescriptionOnly
+  } = item
+  let content = {}
+  if (contents) content = contents[0]
+
+  if (useDescriptionOnly) return description
+  if (contents && (contents.length === 0 || contents.length > 1)) {
+    return description
+  }
+
+  if (content.descriptionOnly) {
+    if (useDescription || description) {
+      return content.description +' '+ description
     }
+    return content.description
 
-    if (contents.length === 0 || contents.length > 1) {
-      return description
+  } else if (content.inventory) {
+    if (useDescription || description) {
+      return content.inventory.name +' '+ description
     }
+    return content.inventory.name
+  }
+}
 
-    if (contents.length === 1) {
+export const contents = (item) => {
+  if (!item) return
+  let contents = []
+  if (item.contents) contents = [...item.contents]
 
+  if (contents.length > 1) {
+
+    return contents.reverse().map(content => {
+
+      if (content.descriptionOnly && content.description) {
+        return <li key={content.id}>{content.description}</li>
+      }
+
+      if (content.inventory && content.description) {
+        return (
+          <li key={content.id}>
+            {`${content.inventory.name} ${content.description}`}
+          </li>
+        )
+      }
+
+      if (content.inventory) {
+        return <li key={content.id}>{content.inventory.name}</li>
+      }
+
+      return <li key={content.id}>{content.description}</li>
+    })
+  }
+}
+
+export const inc = (line) => {
+  if (!line) return
+  if (line.inc) return <CheckSquare width="25" height="25"/>
+  return <Square width="25" height="25"/>
+}
+
+export const quantity = (line) => {
+  if (line) return itemQty(line.item)
+}
+
+export const price = (line, invoiceType, format) => {
+  if (!line) return 0
+
+  //prc factors in lineQty
+  const prc = () => {
+    if (!line.item) return false;
+    let quantity = itemQty(line.item);
+    if (line.quantity) quantity = line.quantity;
+    return sumOfContents(line.item, invoiceType) * quantity;
+  }
+  const totalPrice = prc() - line.discountAdj
+
+  if (format && line.inc) return numeral(0).format('$0,0.00')
+  if (line.inc) return 0
+  if (format) return numeral(totalPrice).format('$0,0.00')
+  return totalPrice
+}
+
+// -------------Item---------------
+
+export const itemPrice = (item, invoiceType, format) => {
+  if (!item) return
+  const qty = itemQty(item)
+  const soc = sumOfContents(item, invoiceType)
+
+  if (format) {
+    if (qty) return numeral(soc * qty).format('$0,0.00');
+    return numeral(soc).format('$0,0.00')
+  }
+
+  if (qty) return soc * qty;
+  return soc
+}
+
+export const itemQty = (item) => {
+  if (!item) return;
+
+  const contents = [...item.contents]
+  let content = {}
+  const oneObj = contents && contents.length === 1
+  if (oneObj) content = contents[0]
+
+  if (item.useQuantity || item.quantity >= 1) {
+    if (content.quantity) return item.quantity * content.quantity
+    return item.quantity
+  }
+
+  if (content.quantity) return content.quantity
+}
+
+// ------------Contents-----------
+
+const sumOfContents = (item, invoiceType) => {
+    const { contents } = item
+
+    //if 1 piece of Content
+    if (contents && contents.length === 1) {
       const content = contents[0]
 
-      if (content.descriptionOnly) {
-        if (useDescription) {
-          return content.description +' '+ description
-        } else {
-          return content.description
-        }
-      } else if (content.inventory) {
-        if (useDescription) {
-          return content.inventory.name +' '+ description
-        } else {
-          return content.inventory.name
-        }
-      }
+      if (content.inc) return 0
+      return inventoryPrice(content, invoiceType) * content.quantity - content.discountAdj
 
     }
-  }
-}
 
-const contents = (item) => {
-  if (item) {
-    const contents = [...item.contents]
+    //if more than 1 piece of content
     if (contents && contents.length > 1) {
-      return contents.reverse().map(content => {
-        if (content.descriptionOnly) {
-
-          if (content.description) {
-            return (
-              <li key={content.id}>{content.description}</li>
-            )
-          }
-
-        } else if (content.inventory) {
-
-          if (content.description) {
-            return (
-              <li key={content.id}>{`${content.inventory.name} ${content.description}`}</li>
-            )
-          } else {
-            return (
-              <li key={content.id}>{content.inventory.name}</li>
-            )
-          }
-
-        }
-        return null
+      const prices = contents.map( content => {
+        return inventoryPrice(content, invoiceType) * content.quantity - content.discountAdj
       })
+
+      return prices.reduce((a, c) => a + c)
     }
+
+    return 0
   }
-}
 
-const inc = (line) => {
-  if (line) {
-    const included = line.inc
+// ------------Inventory----------
 
-    if (included) {
-      return <CheckSquare width="25" height="25"/>
-    } else {
-      return <Square width="25" height="25"/>
-    }
+const inventoryPrice = (content, invoiceType) => {
+  const { inventory } = content
+  if (!inventory) return 0
+
+  if (invoiceType === 'Rental Contract') {
+    return inventory.rentalPrice
   }
-}
 
-const quantity = (line) => {
-  if (line) {
-    const { item } = line
-
-    if (item) {
-      if (item.useQuantity) {
-
-        const contents = [...item.contents]
-        if (contents && contents.length === 1) {
-          const content = contents[0]
-          if (content.quantity) {
-            return item.quantity * content.quantity
-          }
-        } else {
-          return item.quantity
-        }
-
-      } else {
-
-        const contents = [...item.contents]
-        if (contents && contents.length === 1) {
-          const content = contents[0]
-          if (content.quantity) {
-            return content.quantity
-          }
-        }
-
-      }
-    }
-  }
-}
-
-const price = (line, invoiceType, format) => {
-  if (line) {
-    const { item } = line
-
-    const inventoryPrice = (content) => {
-      const { inventory } = content
-      if (inventory) {
-        if (invoiceType === 'Rental Contract') {
-          return inventory.rentalPrice
-        } else {
-          return inventory.sellPrice
-        }
-      } else {
-        return 0
-      }
-    }
-
-    const sumOfContents = () => {
-      const { contents } = item
-
-      if (contents && contents.length === 1) {
-        const content = contents[0]
-        if (content.inc) {
-          return 0
-        } else {
-          return inventoryPrice(content) * content.quantity - content.discountAdj
-        }
-
-      } else if (contents && contents.length > 1) {
-        const prices = contents.map( content => {
-          return inventoryPrice(content) * content.quantity - content.discountAdj
-        })
-        const sum = prices.reduce((a, c) => a + c)
-        return sum
-
-      } else {
-        return 0
-      }
-    }
-
-    const itemPrice = () => {
-      if (item) {
-        let quantity = item.quantity;
-        if (line.quantity) quantity = line.quantity;
-        const subPrice = sumOfContents() * quantity;
-        return subPrice
-      }
-    }
-
-    if (format) {
-      if (line.inc) {
-        return numeral(0).format('$0,0.00')
-      } else {
-        const totalPrice = itemPrice() - line.discountAdj
-        return numeral(totalPrice).format('$0,0.00')
-      }
-    } else {
-      if (line.inc) {
-        return 0
-      } else {
-        const totalPrice = itemPrice() - line.discountAdj
-        return totalPrice
-      }
-    }
-  }
-}
-
-export {
-  description,
-  contents,
-  inc,
-  quantity,
-  price
+  return inventory.sellPrice
 }
