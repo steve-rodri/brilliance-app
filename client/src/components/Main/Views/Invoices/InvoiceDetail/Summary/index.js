0927b69732cdc7357a08973965199330a105fa9a
Steve <steve.rodri@gmail.com>
2019-05-17 11:39:37 -0400

    change summary to display balance and deposit if not proposal

--- client/src/components/Main/Views/Invoices/InvoiceDetail/Summary/index.js
+++ client/src/components/Main/Views/Invoices/InvoiceDetail/Summary/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import numeral from 'numeral'
 import './index.css'
 
@@ -35,24 +35,31 @@ export default class Summary extends Component {
                   <label>Total</label>
                   <p>{numeral(inv.total).format('$0,0.00')}</p>
                 </div>
-                <div>
-                  <label>Deposit</label>
-                  <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
-                    <p>$</p>
-                    <input
-                      className="Summary--input"
-                      type="number"
-                      name="deposit"
-                      value={fields && fields.deposit? fields.deposit : ''}
-                      onChange={(e) => handleChange(e)}
-                      onFocus={this.props.handleFocusSelect}
-                    />
-                  </form>
-                </div>
-                <div>
-                  <label>Balance</label>
-                  <p>{numeral(inv.balance).format('$0,0.00')}</p>
-                </div>
+                {
+                  inv.kind !== 'Proposal'?
+                  <Fragment>
+                    <div>
+                      <label>Deposit</label>
+                      <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
+                        <p>$</p>
+                        <input
+                          className="Summary--input"
+                          type="number"
+                          name="deposit"
+                          value={fields && fields.deposit? fields.deposit : ''}
+                          onChange={(e) => handleChange(e)}
+                          onFocus={this.props.handleFocusSelect}
+                        />
+                      </form>
+                    </div>
+                    <div>
+                      <label>Balance</label>
+                      <p>{numeral(inv.balance).format('$0,0.00')}</p>
+                    </div>
+                  </Fragment>
+                  :
+                  null
+                }
               </div>
             </div>
           </div>
