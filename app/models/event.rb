c45989aa37180c74c539475eca702f87701da5fa
Steve <steve.rodri@gmail.com>
2019-05-21 18:50:07 -0400

    destroy invoice if associated event is deleted

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :client, optional: true
-  has_one :invoice, dependent: :nullify
+  has_one :invoice, dependent: :destroy
   belongs_to :place_location, class_name: 'Place', foreign_key: 'location_id', optional: true
   belongs_to :place_call_location, class_name: 'Place', foreign_key: 'call_location_id', optional: true
   belongs_to :contact_creator, class_name: 'Contact', foreign_key: 'creator_id', optional: true
