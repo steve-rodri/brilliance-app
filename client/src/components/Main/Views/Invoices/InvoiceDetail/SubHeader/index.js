2196074e4a7151b84ed427961db9f71b1973fc67
Steve <steve.rodri@gmail.com>
2019-05-24 01:55:52 -0400

    add auto grid sizing to invoice subheader

--- client/src/components/Main/Views/Invoices/InvoiceDetail/SubHeader/index.js
+++ client/src/components/Main/Views/Invoices/InvoiceDetail/SubHeader/index.js
@@ -5,13 +5,47 @@ import Show from './Show'
 export default function SubHeader(props){
   const { editMode } = props
 
-  if (editMode) {
-    return (
-      <Edit {...props}/>
-    )
-  } else {
-    return (
-      <Show {...props}/>
-    )
+  const styleComp = (name) => {
+    const { editMode } = props
+    let style = {}
+    if (editMode) {
+      switch (name) {
+        case 'About':
+          break;
+        case 'Status':
+          break;
+        case 'Commission':
+          break;
+        default:
+          break;
+      }
+    }
+    return style;
   }
+
+  const styleComponents = () => {
+    const { editMode } = props
+    let style = {
+      gridAutoColumns: "minmax(15.25rem, 1fr)",
+      gridTemplateColumns: "repeat(auto-fit, minmax(15.25rem, auto))",
+    };
+
+    if (editMode) {
+      style.gridAutoColumns = "auto";
+      style.gridAutoRows = "auto";
+      style.gridTemplateColumns = "repeat(auto-fit, minmax(19.75rem, auto))";
+    }
+
+    return style;
+  }
+  return (
+    <section className="SubHeader" style={styleComponents()}>
+      {
+        editMode?
+        <Edit {...props} styleComp={styleComp}/>
+        :
+        <Show {...props} styleComp={styleComp}/>
+      }
+    </section>
+  )
 }
