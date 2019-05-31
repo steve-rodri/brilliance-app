7a37228943197379f2ec9a759c6407b7890a77ab
Steve <steve.rodri@gmail.com>
2019-05-30 21:29:38 -0400

    attempt to stop pull to refresh

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,19 @@ import App from './App';
 import './index.css';
 
 document.body.removeAttribute("class")
+document.querySelector('html').addEventListener("touchstart", function() {
+
+        var clientHeight = this.clientHeight;
+        var scrollHeight = this.scrollHeight;
+        var scrollTop = this.scrollTop;
+        var scroll = scrollHeight - scrollTop;
+        var scrollTo;
+
+        if( scroll === clientHeight ) scrollTo = scrollTop - 1;
+        if( scroll === scrollHeight ) scrollTo = scrollTop + 1;
+
+        if(scrollTo) return this.scrollTop = scrollTo;
+
+    });
 
 ReactDOM.render(<App />, document.getElementById('root'));
