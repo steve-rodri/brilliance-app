b2ee854403275789d4f21ea4c309cfa3963e9611
steverodri91 <steve.rodri@gmail.com>
2020-03-17 19:33:31 -0400

    Add support to set redux viewstate on window resize

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,17 +3,44 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import Admin from "./portals/Admin";
 import Login from "./portals/Login";
 import { useSelector, useDispatch } from "react-redux";
-import { resize } from "./redux";
+import { setMobile, setNav } from "./redux";
 
 const Router = () => {
   const dispatch = useDispatch();
+  const view = useSelector(state => state.view);
+
   useEffect(() => {
-    dispatch(resize());
-    window.addEventListener("resize", dispatch(resize()));
-    return () => {
-      window.removeEventListener("resize", dispatch(resize()));
+    const width = window.innerWidth;
+    const setIntitialViewState = () => {
+      if (width < 750) dispatch(setMobile(true));
+      else dispatch(setMobile(false));
+      if (width > 1000) dispatch(setNav(false));
     };
-  });
+    setIntitialViewState();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const resize = () => {
+      resetView();
+      updateNav();
+    };
+
+    const resetView = () => {
+      const width = window.innerWidth;
+      if ((width < 750) & !view.mobile) dispatch(setMobile(true));
+      else if ((width > 750) & view.mobile) dispatch(setMobile(false));
+    };
+
+    const updateNav = () => {
+      const width = window.innerWidth;
+      if ((width > 1000) & view.nav) dispatch(setNav(false));
+    };
+
+    window.addEventListener("resize", resize);
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, [dispatch, view.mobile, view.nav]);
 
   return (
     <Switch>
--- client/src/redux/actions/viewActions.js
+++ client/src/redux/actions/viewActions.js
@@ -1,32 +1,11 @@
 import * as types from "../constants";
 
-export const resize = () => {
-  return dispatch => {
-    dispatch(resetView());
-    dispatch(updateNav());
-  };
-};
-
-const resetView = () => {
-  return dispatch => {
-    const width = window.innerWidth;
-    if (width < 750) dispatch(setMobile(true));
-    else dispatch(setMobile(false));
-  };
-};
-
-const updateNav = e => {
-  return dispatch => {
-    if (window.innerWidth > 1000) dispatch(setNav(false));
-  };
-};
-
-const setMobile = value => ({
+export const setMobile = value => ({
   type: types.SET_MOBILE,
   payload: value
 });
 
-const setNav = value => ({
+export const setNav = value => ({
   type: types.SET_NAV,
   payload: value
 });
