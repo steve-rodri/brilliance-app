f44b2cd21ba931f94a75034f4009ec242421d612
steverodri91 <steve.rodri@gmail.com>
2020-04-21 16:37:12 -0400

    Searchfield update

--- client/src/components/SearchField/index.js
+++ client/src/components/SearchField/index.js
@@ -1,14 +1,16 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
+import { useTraceableState } from "../../hooks";
 import { isObject, isArray } from "../../helpers";
 import { plusIcon, pencilIcon } from "../../icons";
 import "./index.css";
 
-const SearchField = props => {
+const SearchField = (props) => {
   const resultsContainer = useRef();
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue, prevInputValue] = useTraceableState("");
+  const [results, setResults] = useState([]);
   const [highlightedResult, setHighlightedResult] = useState({
     section: null,
-    index: null
+    index: null,
   });
   const [hoveringResults, setHoveringResults] = useState(false);
   const [fieldActive, setFieldActive] = useState(false);
@@ -16,41 +18,43 @@ const SearchField = props => {
   const showEditButton = useCallback(() => setEditButtonVisible(true), []);
   const hideEditButton = () => setEditButtonVisible(false);
 
-  const handleFocus = e => e.target.select();
+  const handleFocus = (e) => e.target.select();
 
-  const handleKeyDown = e => {
-    const { onEnter, onCreate, inputProps, searchResults } = props;
+  const handleKeyDown = (e) => {
+    const {
+      onEnter,
+      onCreate,
+      resultProps: { formatResult },
+    } = props;
     if (e.key === "Enter") {
-      if (countResults()) {
+      if (countResults() && typeof onEnter === "function") {
         onEnter({
-          selectedResult: searchResults[highlightedResult.index]
+          selectedResult: results[highlightedResult.index],
         });
+        setInputValue(formatResult(results[highlightedResult.index]));
         handleCloseResults();
-      } else if (
-        inputProps.value.length > 2 &&
-        typeof onCreate === "function"
-      ) {
+      } else if (inputValue.length > 2 && typeof onCreate === "function") {
         onCreate();
         handleCloseResults();
       }
     }
   };
 
-  const handleEditButtonPress = e => {
+  const handleEditButtonPress = (e) => {
     const { onEdit } = props;
     e.preventDefault();
     e.stopPropagation();
-    if (typeof edit === "function") onEdit();
+    if (typeof onEdit === "function") onEdit();
   };
 
-  const handleOpenResults = value => {
+  const handleOpenResults = (value) => {
     if (value.length > 2) setFieldActive(true);
     else setFieldActive(false);
   };
 
   const handleCloseResults = () => setFieldActive(false);
 
-  const handleScrollResults = direction => {
+  const handleScrollResults = (direction) => {
     const { section, index, height } = highlightedResult;
     const scrollPosition = resultsContainer.current.scrollTop;
     if (direction === "up") {
@@ -72,108 +76,152 @@ const SearchField = props => {
     }
   };
 
-  const handleChangeHighlightedResult = direction => {
+  const handleChangeHighlightedResult = ({ direction, result }) => {
     const { section, index } = highlightedResult;
-    const { searchResults } = props;
-    const isArray = Array.isArray(searchResults);
-    const isObj = searchResults === Object(searchResults) && !isArray;
-
-    if (isObj) {
-      const sections = Object.entries(searchResults);
-      if (direction === "up") {
-        if (index > 0) {
-          setHighlightedResult(prevState => ({
-            ...prevState,
-            index: (prevState.index -= 1)
-          }));
-        } else if (section.index > 0) {
-          setHighlightedResult(prevState => {
-            const prevSection = prevState.section;
-            if (prevSection.index > 0) {
-              const index = sections[prevSection.index - 1][1].length - 1;
+    const isArray = Array.isArray(results);
+    const isObj = results === Object(results) && !isArray;
+    if (result) setHighlightedResult({ ...result });
+    if (direction) {
+      if (isObj) {
+        const sections = Object.entries(results);
+        if (direction === "up") {
+          if (index > 0) {
+            setHighlightedResult((prevState) => ({
+              ...prevState,
+              index: (prevState.index -= 1),
+            }));
+          } else if (section.index > 0) {
+            setHighlightedResult((prevState) => {
+              const prevSection = prevState.section;
+              if (prevSection.index > 0) {
+                const index = sections[prevSection.index - 1][1].length - 1;
+                return {
+                  ...prevState,
+                  section: {
+                    ...prevSection,
+                    index: (prevSection.index -= 1),
+                  },
+                  index,
+                };
+              } else return null;
+            });
+          }
+        }
+        if (direction === "down") {
+          if (index < sections[section.index][1].length - 1) {
+            setHighlightedResult((prevState) => ({
+              ...prevState,
+              index: (prevState.index += 1),
+            }));
+          } else if (section.index + 1 < sections.length) {
+            setHighlightedResult((prevState) => {
+              const prevSection = prevState.section;
               return {
                 ...prevState,
                 section: {
                   ...prevSection,
-                  index: (prevSection.index -= 1)
+                  index: (prevSection.index += 1),
                 },
-                index
+                index: 0,
               };
-            } else return null;
-          });
+            });
+          }
         }
       }
-      if (direction === "down") {
-        if (index < sections[section.index][1].length - 1) {
-          setHighlightedResult(prevState => ({
-            ...prevState,
-            index: (prevState.index += 1)
-          }));
-        } else if (section.index + 1 < sections.length) {
-          setHighlightedResult(prevState => {
-            const prevSection = prevState.section;
-            return {
+      if (isArray) {
+        if (direction === "up") {
+          if (index > 0) {
+            setHighlightedResult((prevState) => ({
               ...prevState,
-              section: {
-                ...prevSection,
-                index: (prevSection.index += 1)
-              },
-              index: 0
-            };
-          });
+              index: (prevState.index -= 1),
+            }));
+          }
         }
-      }
-    } else if (isArray) {
-      if (direction === "up") {
-        if (index > 0) {
-          setHighlightedResult(prevState => ({
-            ...prevState,
-            index: (prevState.index -= 1)
-          }));
-        }
-      }
-      if (direction === "down") {
-        if (index < searchResults.length - 1) {
-          setHighlightedResult(prevState => ({
-            ...prevState,
-            index: (prevState.index += 1)
-          }));
+        if (direction === "down") {
+          if (index < results.length - 1) {
+            setHighlightedResult((prevState) => ({
+              ...prevState,
+              index: (prevState.index += 1),
+            }));
+          }
         }
       }
     }
   };
 
   const countResults = () => {
-    const { searchResults } = props;
-    if (!searchResults) return 0;
-    if (isArray(searchResults)) return searchResults.length;
+    if (!results) return 0;
+    if (isArray(results)) return results.length;
 
     let resultCount = 0;
-    const results = Object.entries(searchResults);
-    results.forEach(obj => {
+    const entries = Object.entries(results);
+    entries.forEach((obj) => {
       if (obj[1] && obj[1].length) resultCount += obj[1].length;
     });
     return resultCount;
   };
 
+  const getResults = useCallback(async () => {
+    const { resultProps } = props;
+    if (!resultProps.getResults && typeof resultProps.getResults !== "function")
+      return;
+    const data = await resultProps.getResults({ q: inputValue });
+    if (data && data.length) setResults(data);
+  }, [props, inputValue]);
+
+  const fetchResults = useCallback(
+    async (isSubscribed) => {
+      if (!inputValue || !isSubscribed) return;
+      const inputValueLength = inputValue.split("").length;
+      if (inputValueLength > 2) {
+        if (prevInputValue) {
+          const prevInputValueLength = prevInputValue.split("").length;
+          if (inputValueLength > prevInputValueLength) await getResults();
+        } else {
+          await getResults();
+        }
+      }
+    },
+    [inputValue, prevInputValue, getResults]
+  );
+
+  const eraseFormValue = useCallback(() => {
+    if (props.deleteFormValue && typeof props.deleteFormValue === "function") {
+      props.deleteFormValue();
+    }
+  }, [props]);
+
   //value Change
   useEffect(() => {
-    setHighlightedResult(prevState => ({
+    let isSubscribed = true;
+    fetchResults(isSubscribed);
+    setHighlightedResult((prevState) => ({
       ...prevState,
       section: {
         ...prevState.section,
-        index: 0
+        index: 0,
       },
-      index: 0
+      index: 0,
     }));
-    setInputValue(props.value);
-  }, [props.value]);
-  //searchResults Change
+    return () => (isSubscribed = false);
+  }, [inputValue, fetchResults]);
+
+  useEffect(() => {
+    setInputValue(props.defaultValue || "");
+  }, [props.defaultValue, setInputValue]);
+
+  useEffect(() => {
+    if (inputValue !== prevInputValue && inputValue !== props.defaultValue) {
+      if (props.defaultValue) eraseFormValue();
+    }
+  }, [prevInputValue, inputValue, props.defaultValue, eraseFormValue]);
+
+  //results Change
   useEffect(() => {
     if (resultsContainer.current) {
       resultsContainer.current.scrollTop = 0;
     }
-  }, [props.searchResults]);
+  }, [results]);
   //scroll Change
   useEffect(() => {
     setFieldActive(false);
@@ -183,28 +231,36 @@ const SearchField = props => {
     if (props.formDataValue) showEditButton();
     else hideEditButton();
   }, [props.formDataValue, showEditButton]);
+  //restore highlightedResult
+  useEffect(() => {
+    if (!hoveringResults) {
+      setHighlightedResult({ index: 0, section: { index: 0 } });
+    }
+  }, [hoveringResults]);
 
   return (
     <div className="SearchField">
       <form
         autoComplete="off"
-        className={props.formClassName}
-        onSubmit={e => e.preventDefault()}
+        className={props.className}
+        onSubmit={(e) => e.preventDefault()}
         onFocus={handleFocus}
         onKeyDown={handleKeyDown}
       >
         <Input
           {...props}
           resultProps={{
+            ...props.resultProps,
             hoveringResults,
             openResults: handleOpenResults,
             closeResults: handleCloseResults,
             scrollResults: handleScrollResults,
-            changeHighlightedResult: handleChangeHighlightedResult
+            changeHighlightedResult: handleChangeHighlightedResult,
           }}
           inputProps={{
             ...props.inputProps,
-            value: inputValue
+            value: inputValue,
+            setInputValue,
           }}
         />
         <ResultsContainer
@@ -212,12 +268,18 @@ const SearchField = props => {
           containerRef={resultsContainer}
           fieldActive={fieldActive}
           resultProps={{
+            ...props.resultProps,
+            results,
             highlightedResult,
             setHoveringResults,
             closeResults: handleCloseResults,
             changeHighlightedResult: handleChangeHighlightedResult,
-            formatResult: props.formatResult,
-            count: countResults
+            resultCount: countResults(),
+          }}
+          inputProps={{
+            ...props.inputProps,
+            value: inputValue,
+            setInputValue,
           }}
         />
       </form>
@@ -226,38 +288,40 @@ const SearchField = props => {
   );
 };
 
-const Input = ({ inputProps, onChange, formDataValue, ...rest }) => {
+const Input = ({ inputProps, resultProps, formDataValue, ...rest }) => {
   const {
     openResults,
     closeResults,
     scrollResults,
     changeHighlightedResult,
-    hoveringResults
-  } = rest.resultProps;
-  const handleKeyDown = e => {
+    hoveringResults,
+  } = resultProps;
+  const { value: inputValue, setInputValue, ...restInputProps } = inputProps;
+  const handleKeyDown = (e) => {
     if (e.key === "ArrowDown") {
       e.preventDefault();
-      changeHighlightedResult("down");
+      changeHighlightedResult({ direction: "down" });
 
       scrollResults("down");
     } else if (e.key === "ArrowUp") {
       e.preventDefault();
-      changeHighlightedResult("up");
+      changeHighlightedResult({ direction: "up" });
 
       scrollResults("up");
     }
   };
-  const handleBlur = e => {
-    if (inputProps.value && !formDataValue && !hoveringResults) onChange("");
+  const handleBlur = (e) => {
+    if (inputValue && !formDataValue && !hoveringResults) setInputValue("");
     if (!hoveringResults) closeResults();
   };
   const handleChange = ({ target: { value } }) => {
-    onChange(value);
+    setInputValue(value);
     openResults(value);
   };
   return (
     <input
-      {...inputProps}
+      {...restInputProps}
+      value={inputValue}
       onFocus={openResults}
       onChange={handleChange}
       onBlur={handleBlur}
@@ -266,39 +330,47 @@ const Input = ({ inputProps, onChange, formDataValue, ...rest }) => {
   );
 };
 
-const ResultsContainer = props => {
+const ResultsContainer = (props) => {
+  const {
+    resultProps: { setHoveringResults },
+    fieldActive,
+    containerRef,
+  } = props;
   const styleResultsContainer = () => {
-    const { searchResults, resultProps } = props;
+    const { results, resultCount } = props.resultProps;
     let style = {};
-    if (isArray(searchResults)) {
-      if (!searchResults.length && props.mobile) {
+    if (isArray(results)) {
+      if (!results.length && props.mobile) {
         style.height = "60px";
-      } else if (!searchResults.length) {
+      } else if (!results.length) {
         style.height = "50px";
       }
-    } else if (!resultProps.resultCount) {
+    } else if (!resultCount) {
       style.height = "70px";
     }
     return style;
   };
-  const leavingResults = e => {
+  const leavingResults = (e) => {
     e.stopPropagation();
-    props.resultProps.setHoveringResults(false);
+    setHoveringResults(false);
   };
-  const choosingResult = e => {
+  const choosingResult = (e) => {
     e.stopPropagation();
-    props.resultProps.setHoveringResults(true);
+    setHoveringResults(true);
   };
   const render = () => {
-    if (!props.resultProps.count()) return <CreateNewFromResult {...props} />;
-    if (isObject(props.searchResults)) return <SectionedResults {...props} />;
-    else return <Results {...props} />;
+    const { results, resultCount } = props.resultProps;
+    if (!resultCount)
+      return <CreateNewFromResult {...props} {...props.resultProps} />;
+    if (isObject(results))
+      return <SectionedResults {...props} {...props.resultProps} />;
+    else return <Results {...props} {...props.resultProps} />;
   };
-  if (!props.fieldActive) return null;
+  if (!fieldActive) return null;
   return (
     <div
       className={`SearchField--results-container`}
-      ref={props.containerRef}
+      ref={containerRef}
       style={styleResultsContainer()}
       onMouseEnter={choosingResult}
       onMouseLeave={leavingResults}
@@ -308,27 +380,27 @@ const ResultsContainer = props => {
   );
 };
 
-const SectionedResults = ({ searchResults, ...rest }) => {
-  const sections = Object.entries(searchResults);
+const SectionedResults = ({ results, ...rest }) => {
+  const sections = Object.entries(results);
   return sections.map((entry, index) => {
     const section = { index, name: entry[0], results: entry[1] };
     return <ResultsSection section={section} {...rest} />;
   });
 };
 
-const ResultsSection = ({ section, sectionTitleClassName }) => {
+const ResultsSection = ({ section, sectionTitleClassName, ...rest }) => {
   return (
     <>
       <SectionTitle className={sectionTitleClassName} section={section} />
       {section.results.map((result, i) => (
-        <Result />
+        <Result key={result.id || i} result={result} index={i} {...rest} />
       ))}
     </>
   );
 };
 
 const SectionTitle = ({ className, section }) => {
-  const styleSectionTitle = section => {
+  const styleSectionTitle = (section) => {
     let style = {};
     if (section.index > 0) {
       style.top = `${section.index * 28}px`;
@@ -346,52 +418,55 @@ const SectionTitle = ({ className, section }) => {
   );
 };
 
-const Results = ({ searchResults, ...rest }) => {
-  return searchResults.map((result, i) => (
+const Results = ({ results = [], ...rest }) => {
+  return results.map((result, i) => (
     <Result key={result.id || i} result={result} index={i} {...rest} />
   ));
 };
 
-const Result = ({
-  resultClassName: className,
-  result,
-  index,
-  section,
-  resultProps,
-  inputProps,
-  onSelect
-}) => {
-  const handleSelect = e => {
+const Result = ({ result = {}, ...rest }) => {
+  const {
+    index,
+    section,
+    resultClassName: className,
+    formatResult,
+    changeHighlightedResult,
+    closeResults,
+    onSelect,
+    inputProps: { setInputValue },
+  } = rest;
+  const handleSelect = (e) => {
     e.stopPropagation();
     onSelect({ selectedResult: result });
-    resultProps.closeResults();
+    setInputValue(formatResult(result));
+    closeResults();
   };
-  const handleMouseEnter = e => resultProps.changeHighlightedResult(e, index);
+  const handleMouseEnter = () =>
+    changeHighlightedResult({ result: { section, index } });
   const styleResult = (index, section) => {
     const {
-      hoveringResults,
-      highlightedResult: { section: highSection, index: highIndex }
-    } = resultProps;
+      resultProps: {
+        highlightedResult: { section: highSection, index: highIndex },
+      },
+    } = rest;
     const style = {};
-    if (!hoveringResults) {
-      if (section) {
-        if (section.index === highSection.index && index === highIndex)
-          style.backgroundColor = "var(--light-blue)";
-      } else {
-        if (index === highIndex) style.backgroundColor = "var(--light-blue)";
-      }
+    if (section) {
+      if (section.index === highSection.index && index === highIndex)
+        style.backgroundColor = "var(--light-blue)";
+    } else {
+      if (index === highIndex) style.backgroundColor = "var(--light-blue)";
     }
     return style;
   };
   return (
     <div
       style={styleResult(index, section)}
-      key={(result && result.id) || index}
+      key={result.id || index}
       className={`${className} SearchField--result`}
       onClick={handleSelect}
       onMouseEnter={handleMouseEnter}
     >
-      <>{resultProps.formatResult(result, section)}</>
+      <>{formatResult(result, section)}</>
     </div>
   );
 };
@@ -400,9 +475,9 @@ const CreateNewFromResult = ({ onCreate, label, ...props }) => {
   if (typeof onCreate !== "function") return null;
   const {
     resultProps: { closeResults },
-    inputProps: { value }
+    inputProps: { value },
   } = props;
-  const handleCreate = e => {
+  const handleCreate = (e) => {
     e.stopPropagation();
     onCreate();
     closeResults();
